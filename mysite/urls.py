f0968123f25d4a3a47f35ac0393156f472eaa8e9
shayanJR99 <shahrbandishayan@gmail.com>
2026-02-17 22:06:37 +0330

    shayanJR: added 2 pages to     mysite.urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from website.views import about_us,home
 
 urlpatterns = [
+    path("",home),
     path('admin/', admin.site.urls),
+    path('about_us', about_us)
 ]
