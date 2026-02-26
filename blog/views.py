a79678dc5aae79cea5d3b1f84ddbfcdd6f1946ea
shayanJR99 <shahrbandishayan@gmail.com>
2026-02-27 03:00:54 +0330

    shayanJR: given data to blog.views.home_view and used it in blog_home.html

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_view(request):
-    return render(request, "blog/blog-home.html")
+    data = {"author":"shayanJR",
+            "title" : "buy bitcoin",
+            "paragraph":"buy bitcoin because it reached from 120,000 to 64,000 !!!"
+            }
+    
+    return render(request, 
+                  "blog/blog-home.html",
+                  data
+                  )
 
 def blog_single(request):
     return render(request, "blog/blog-single.html")
